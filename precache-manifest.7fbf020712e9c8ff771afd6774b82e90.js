8e1659dacc9782395418456821df5d1131b6e204
Supriya <sup2906@gmail.com>
2020-07-23 22:19:29 +0530

    Updates

---
+++ precache-manifest.7fbf020712e9c8ff771afd6774b82e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd91cba0d85a417d2858563bd02bb16c",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "b004d99e03aa4816d09a",
+    "url": "/project/static/css/main.f9d29a06.chunk.css"
+  },
+  {
+    "revision": "5201c08cbd97b13c93d5",
+    "url": "/project/static/js/2.0a6b0dca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project/static/js/2.0a6b0dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b004d99e03aa4816d09a",
+    "url": "/project/static/js/main.bdebebd5.chunk.js"
+  },
+  {
+    "revision": "76503f7718340313a182",
+    "url": "/project/static/js/runtime-main.e98d2cc5.js"
+  }
+]);
